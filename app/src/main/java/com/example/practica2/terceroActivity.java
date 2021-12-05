186d2b9e719cb17761343094c0c43b00e6ccdbac
fabiolacuray <fabiolacuray07@gmail.com>
2021-12-05 17:29:22 -0500

    primer commit

---
+++ app/src/main/java/com/example/practica2/terceroActivity.java
@@ -0,0 +1,14 @@
+package com.example.practica2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class terceroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tercero);
+    }
+}
